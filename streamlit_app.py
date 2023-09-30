0647e4e3c659b29cb46de18670fa3ab31d409451
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 00:34:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ selected_disease = st.sidebar.selectbox(
     "Please select a disease :", disease_list
 )
 
-data = fetch_data("select NCTID from MASTER_DATA WHERE disease = " + selected_disease + " LIMIT 10")
+query = "select NCTID from MASTER_DATA WHERE disease = " + selected_disease + " LIMIT 10"
+data = fetch_data(query)
 
 st.title('🦜🔗 Quickstart App')
 st.dataframe(data)
