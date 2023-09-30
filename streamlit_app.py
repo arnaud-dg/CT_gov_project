14181fb345a1b7404b6aa369fcb9a3533f7ae4ed
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 00:01:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from available_diseases")
 my_data_row = my_cur.fetchone()
