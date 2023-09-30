fcbfe1f27802fb5ac6d7c58a9ea37fb39f6c6237
arnaud_dg <ad_learning@yahoo.com>
2023-09-30 23:59:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,30 +6,10 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from available_diseases")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "Please select a disease :", my_data_row
 )
 
-st.header("Tableau de données")
-st.dataframe(data) 
-
 st.title('🦜🔗 Quickstart App')
-
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
-
-with st.form('my_form'):
-    text = st.text_area('Enter text:', 'WHat is the definition of a clinical trial ?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
