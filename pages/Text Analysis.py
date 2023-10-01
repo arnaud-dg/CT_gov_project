ca1575f74e411a9849ec60a21d3737af3840e7e0
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 22:46:10 +0200

    Update Text Analysis.py

--- pages/Text Analysis.py
+++ pages/Text Analysis.py
@@ -32,7 +32,7 @@ st.header('Text analysis of the Study Description')
 st.dataframe(df_nlp)
 
 # Create some sample text
-text = df['colonne_string'].str.cat(sep=' ')
+text = df['Text_field'].str.cat(sep=' ')
 text = text.split(" ")
 
 # Create and generate a word cloud image:
