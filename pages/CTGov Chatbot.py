75a32ac7cdb641b79d5db5d3b6d3a89117531254
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 23:12:50 +0200

    Update CTGov Chatbot.py

--- pages/CTGov Chatbot.py
+++ pages/CTGov Chatbot.py
@@ -1,5 +1,28 @@
-import streamlit as st
 import openai
+import pandas as pd
+import streamlit as st
+import snowflake.connector
+import altair as alt
+import plotly.express as px
+import leafmap.foliumap as leafmap
+
+# Importing function
+def fetch_data(SQL_query):
+    # Connection to snowflake and cursor creation
+    conn = snowflake.connector.connect(**st.secrets["snowflake"])
+    cur = conn.cursor()
+    cur.execute(SQL_query)
+    # Loading Data into a DataFrame
+    df = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+    # Close the connection
+    cur.close()
+    conn.close()
+    return df
+
+# Drop-down list of the sidebar
+df_disease = fetch_data("select $1 from available_diseases")
+selected_disease = st.sidebar.selectbox("Please select a disease :", df_disease['$1'].tolist())
+st.sidebar.write("Vous avez choisi : ", selected_disease)
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
