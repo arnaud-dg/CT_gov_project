a7c3b9808b181b31b745d25fbe2a4e305663537b
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 11:54:55 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -33,13 +33,11 @@ st.sidebar.write("Vous avez choisi : ", selected_disease)
 query = "select NCTID from MASTER_DATA WHERE disease = '" + selected_disease + "' LIMIT 10"
 data = fetch_data(query)
 
-df = fetch_data("SELECT count(*) FROM studies_count WHERE disease = '" + selected_disease + "'")
-
 st.title('🏥 Clinical Trials .Gov Explorer 🧑‍⚕️')
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total number of studies", fetch_data("SELECT count(*) FROM studies_count WHERE disease = '" + selected_disease + "'").iloc[0, 0])
-col2.metric("On-going Clinical studies", "0 mph", "-8%")
-col3.metric("Completed Clinical studies", "86%", "4%")
-col4.metric("Number of lines", "86%", "4%")
+col2.metric("On-going Clinical studies", fetch_data("SELECT count(*) FROM studies_count WHERE disease = '" + selected_disease + "' AND simplifiedstatus = 'On-going'").iloc[0, 0])
+col3.metric("Completed Clinical studies", fetch_data("SELECT count(*) FROM studies_count WHERE disease = '" + selected_disease + "' AND simplifiedstatus = 'Closed'").iloc[0, 0])
+col4.metric("Number of lines", fetch_data("SELECT count(*) FROM studies_count WHERE disease = '" + selected_disease + "' AND simplifiedstatus = 'Unknown'").iloc[0, 0])
 
 st.dataframe(df)
