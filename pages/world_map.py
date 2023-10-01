ae9cbfcd41330a4b1ba58a961be916f313ea8230
arnaud_dg <ad_learning@yahoo.com>
2023-10-01 14:02:11 +0200

    Update world_map.py

--- pages/world_map.py
+++ pages/world_map.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import streamlit as st
+import snowflake.connector
+import altair as alt
 
+# Importing function
+def fetch_data(SQL_query):
+    # Connection to snowflake and cursor creation
+    conn = snowflake.connector.connect(**st.secrets["snowflake"])
+    cur = conn.cursor()
+    cur.execute(SQL_query)
+    # Loading Data into a DataFrame
+    df = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+    # Close the connection
+    cur.close()
+    conn.close()
+    return df
+
+# Drop-down list of the sidebar
+df_disease = fetch_data("select $1 from available_diseases")
+selected_disease = st.sidebar.selectbox("Please select a disease :", df_disease['$1'].tolist())
+st.sidebar.write("Vous avez choisi : ", selected_disease)
+
+query = "select NCTID from MASTER_DATA WHERE disease = '" + selected_disease + "' LIMIT 10"
+data = fetch_data(query)
+
+st.title('🏥 Clinical Trials .Gov Explorer 🧑‍⚕️')
