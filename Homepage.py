e59f3c94a54e65afc781a740c1749dc8287ddabf
arnaud_dg <ad_learning@yahoo.com>
2023-10-03 00:26:21 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -10,6 +10,37 @@ from streamlit_extras import extra
 st.set_page_config(layout="wide")
 add_logo("gallery/transparent_logo_DATA_BOOST_2.png", height=300)
 
+@extra
+def style_metric_cards(
+    background_color: str = "#FFF",
+    border_size_px: int = 1,
+    border_color: str = "#CCC",
+    border_radius_px: int = 5,
+    border_left_color: str = "#9AD8E1",
+    box_shadow: bool = True,
+):
+
+    box_shadow_str = (
+        "box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;"
+        if box_shadow
+        else "box-shadow: none !important;"
+    )
+    st.markdown(
+        f"""
+        <style>
+            div[data-testid="metric-container"] {{
+                background-color: {background_color};
+                border: {border_size_px}px solid {border_color};
+                padding: 5% 5% 5% 10%;
+                border-radius: {border_radius_px}px;
+                border-left: 0.5rem solid {border_left_color} !important;
+                {box_shadow_str}
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 # Importing function
 def fetch_data(SQL_query):
     # Connection to snowflake and cursor creation
