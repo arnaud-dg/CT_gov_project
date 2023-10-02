227184eadd703566ea906edcefbfc62da902166f
arnaud_dg <ad_learning@yahoo.com>
2023-10-03 00:20:37 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -41,6 +41,7 @@ col1.metric("Total number of studies", df_metrics['COUNT'].sum())
 col2.metric("On-going Clinical studies", df_metrics[df_metrics['SIMPLIFIEDSTATUS'] == 'On-going']['COUNT'].sum())
 col3.metric("Completed Clinical studies", df_metrics[df_metrics['SIMPLIFIEDSTATUS'] == 'Closed']['COUNT'].sum())
 col4.metric("Number of lines", df_metrics[df_metrics['SIMPLIFIEDSTATUS'] == 'Unknown']['COUNT'].sum())
-style_metric_cards(border_left_color="#9AD8E1")
+style_metric_cards(background_color='#FFF', border_size_px=1, border_color='#CCC', border_radius_px=5, border_left_color="#9AD8E1")
+
 
 
