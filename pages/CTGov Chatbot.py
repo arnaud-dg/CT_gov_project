d6bdec24481e64ef7eef04637847ce8523d003b3
arnaud_dg <ad_learning@yahoo.com>
2023-10-04 00:53:55 +0200

    Update CTGov Chatbot.py

--- pages/CTGov Chatbot.py
+++ pages/CTGov Chatbot.py
@@ -35,3 +35,7 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+conn = st.experimental_connection("snowpark")
+df = conn.query("select current_warehouse()")
+st.write(df)
