2cc5d71f67fb2c0a2234668943f08c3eda992b75
arnaud_dg <ad_learning@yahoo.com>
2023-10-04 01:10:21 +0200

    Update CTGov Chatbot.py

--- pages/CTGov Chatbot.py
+++ pages/CTGov Chatbot.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 import snowflake.connector
 import pandas as pd
-from llm-chatbot.prompts import get_system_prompt
+from prompts import get_system_prompt
 
 st.title("☃️ Frosty")
 
